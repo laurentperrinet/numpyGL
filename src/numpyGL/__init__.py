81fb1022a538b9816ea180e14f05c167757a2314
Laurent Perrinet <laurent.perrinet@gmail.com>
2016-02-02 16:22:04 +0100

    making a package : structure

---
+++ src/numpyGL/__init__.py
@@ -0,0 +1,6 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, absolute_import
+__author__ = "Laurent Perrinet INT - CNRS"
+__version__ = '0.1'
+from .numpyGL import *
