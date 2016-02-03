1a077ac22d0b1beb03d74258255c27ac664ccf07
Laurent Perrinet <laurent.perrinet@gmail.com>
2016-02-03 09:45:22 +0100

    fine tuning use of gloo functions

--- src/numpyGL/numpyGL.py
+++ src/numpyGL/numpyGL.py
@@ -5,7 +5,7 @@
 A simple framework to display numpy array on a screen. And only that.
 
 Heavily based on https://github.com/vispy/vispy/blob/master/examples/tutorial/gloo/textured_quad.py
-by Nicolas Rougier.
+by Nicolas Rougier and more example scripts in the vispy documentation.
 
 """
 from __future__ import division, print_function, absolute_import
@@ -16,19 +16,23 @@ __licence__ = 'BSD licence'
 import numpy as np
 import time
 
-# VISUALIZATION ROUTINES
 from vispy import app
 from vispy import gloo
-
 #--------------------------------------------------------------------------
+# A simple texture quad
+data = np.zeros(4, dtype=[('a_position', np.float32, 2),
+                          ('a_texcoord', np.float32, 2)])
+data['a_position'] = np.array([[-1, -1], [+1, -1], [-1, +1], [+1, +1]])
+data['a_texcoord'] = np.array([[1, 0], [1, 1.], [0, 0], [0, 1.]])
+
 
 vertex = """
-    attribute vec2 position;
+    attribute vec2 a_position;
     attribute vec2 a_texcoord;
     varying vec2 v_texcoord;
     void main()
     {
-        gl_Position = vec4(position, 0.0, 1.0);
+        gl_Position = vec4(a_position, 0.0, 1.0);
         v_texcoord = a_texcoord;
     }
 """
@@ -48,7 +52,7 @@ class Canvas(app.Canvas):
     camera take will occur.
     """
     def __init__(self, stimulus, 
-            fullscreen=True, 
+            fullscreen=True, interpolation='linear',
             timeline=np.linspace(0, 4, 4*30), keys='interactive', title='welcome to numpyGL'):
         self.stimulus = stimulus
         self.timeline = timeline
@@ -56,9 +60,11 @@ class Canvas(app.Canvas):
         super(Canvas, self).__init__(keys=keys, title=title)
         width, height = self.physical_size
         self.program = gloo.Program(vertex, fragment, count=4)
-        self.program['position'] = [(-1, -1), (-1, +1), (+1, -1), (+1, +1)]
-        self.program['a_texcoord'] = [(1, 1), (1, 0), (0, 1), (0, 0)]
-        self.program['texture'] = self.stimulus(t=0.)
+        self.program.bind(gloo.VertexBuffer(data))
+        #self.program['a_position'] = [(-1, -1), (-1, +1), (+1, -1), (+1, +1)]
+        #self.program['a_texcoord'] = [(1, 1), (1, 0), (0, 1), (0, 0)]
+        #self.program['texture'] = self.stimulus(t=0.)
+        self.program['texture'] = gloo.Texture2D(self.stimulus(t=0.), interpolation=interpolation)
         gloo.set_viewport(0, 0, width, height)
         self._timer = app.Timer('auto', connect=self.on_timer, start=True)
         self.start = time.time() # use the timer above
